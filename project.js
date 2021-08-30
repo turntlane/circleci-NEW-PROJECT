856438817e725226e868d8870af78d0e9a7f4f22
Jake Alexander <imjakeal123@gmail.com>
2021-08-30 16:15:18 -0600

    idk man

--- project.js
+++ project.js
@@ -1,11 +1,21 @@
-function getGreeting(name){
-    return "Hi " + name;
-}
+
 
 
 // function add(num1, num2) {
 //     return num1 + num2
 // }
 
-module.exports = getGreeting;
+// module.exports = getGreeting;
 // module.exports = add;
+
+
+
+module.exports = {
+    getGreeting: (name) => {
+        return "Hi " + name;
+    },
+
+    add: (num1, num2) => {
+        return num1 + num2;s
+    }
+}
--- test.js
+++ test.js
@@ -1,10 +1,9 @@
-const getGreeting = require('./project');
-// const add = require('./project');
+const {getGreeting, add} = require('./project');
 
 test('can create a basic greeting', () => {
   expect(getGreeting('Sam')).toBe("Hi Sam");
 });
 
-// test('add', () => {
-//   expect(add(1, 2)).toEqual(3)
-// })
+test('add', () => {
+  expect(add(1, 2)).toEqual(3)
+})
